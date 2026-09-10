e98a95b3ad3b34da0b8aba8ab7b30721da4a4b4e
Wyuio-0 <wyuio_0@qq.com>
2026-09-10 19:59:39 +0800

    fix(android): 补充 MainActivity 中的 RoundedCornerShape 导入

--- android/app/src/main/java/com/amiya/pet/ui/MainActivity.kt
+++ android/app/src/main/java/com/amiya/pet/ui/MainActivity.kt
@@ -14,6 +14,7 @@ import androidx.activity.compose.setContent
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.*
