1b0af4164720626d51c05b3a6e5e482afb3ba0e3
Wyuio-0 <wyuio_0@qq.com>
2026-09-19 15:12:23 +0800

    chore(release): bump version to v1.9.2 and update CHANGELOG

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.amiya.pet"
         minSdk = 26
         targetSdk = 35
-        versionCode = 211
-        versionName = "1.9.1"
+        versionCode = 212
+        versionName = "1.9.2"
     }
 
     signingConfigs {
